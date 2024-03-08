bee6d3ba8039a536d8b49810f7a3ee69f07667e6
Amado <amado.2002@hotmail.com>
2024-03-08 01:41:07 -0500

    Registrar a medias

--- frontend/src/app/components/pages/create-account/create-account.component.ts
+++ frontend/src/app/components/pages/create-account/create-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-create-account',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-account.component.scss']
 })
 export class CreateAccountComponent implements OnInit {
+  generoSeleccionado: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log("iniciando componente");
+  }
+  
+  onGeneroChange() {
+
+    console.log('logrado');
+    
+}
+  continuarBtn() {
+    console.log(this.generoSeleccionado); 
   }
 
 }
